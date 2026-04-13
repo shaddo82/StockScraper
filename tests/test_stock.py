a1fd2d87aa6a6f3ebce78cf332d36069e743c8a0
황재민 <ghkd1791@gmail.com>
2026-04-13 18:54:53 +0900

    add simple test

---
+++ tests/test_stock.py
@@ -0,0 +1,11 @@
+# tests/test_stock.py
+
+def calculate_price_change(current, previous):
+    """가격 변화율 계산"""
+    return ((current - previous) / previous) * 100
+
+
+def test_price_change():
+    """가격 변화율이 올바르게 계산되는지 테스트"""
+    result = calculate_price_change(110, 100)
+    assert result == 10.0
